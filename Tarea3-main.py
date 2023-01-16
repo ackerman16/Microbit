5a1f9950aa60015644f65bf4c718016806af7db8
Gumball <114919501+ackerman16@users.noreply.github.com>
2023-01-16 13:38:18 +0100

    Add files via upload

---
+++ Tarea3-main.py
@@ -0,0 +1,14 @@
+# Imports go at the top
+from microbit import *
+
+contador = 0
+
+while True:
+    if button_a.get_presses():
+        contador = contador + 1
+    elif button_b.get_presses():
+        contador = contador - 1
+    else:
+        display.show(contador)
+
+
