b50d8092899dbfb5eb5c84c35c3374292dcfa6ec
Gumball <114919501+ackerman16@users.noreply.github.com>
2023-02-01 13:22:45 +0100

    Add files via upload

---
+++ Ejemplo5.py
@@ -0,0 +1,11 @@
+# Imports go at the top
+from microbit import *
+
+lista_vocales = ["A","E","I","O","U"]
+
+index = 0
+
+while( index < len(lista_vocales)):
+    display.show(lista_vocales[index])
+    index = index + 1
+    sleep(1000)
