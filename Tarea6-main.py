c8ee458a2c2f23824d0bcb4aa40e48efb186cb73
Gumball <114919501+ackerman16@users.noreply.github.com>
2023-02-13 12:52:01 +0100

    Add files via upload

---
+++ Tarea6-main.py
@@ -0,0 +1,7 @@
+# Imports go at the top
+from microbit import *
+
+import music
+
+Notes = ['g4:4', 'e:4', ' ', 'g4', 'g4', 'a4', 'b4', 'c5', 'd5:3', ' ','d5', 'd5', 'Ee5', 'f5#', 'g5:4',' ', 'g5', 'g5', 'f5#', 'e5#', 'f5#', 'e5', 'd5']
+music.play(Notes)
